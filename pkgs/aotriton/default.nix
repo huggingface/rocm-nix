a72253ff3181958cc49fe2703b3185f9140ad269
Daniël de Kok <me@danieldk.eu>
2025-03-17 19:47:30 +0000

    aotriton_0_9: init at 0.9.2b

--- pkgs/aotriton/default.nix
+++ pkgs/aotriton/default.nix
@@ -1,246 +1,61 @@
-# Vendored from: https://github.com/NixOS/nixpkgs/pull/367695
-# Sync with upstream after it gets merged.
 {
-  lib,
-  stdenv,
+  callPackage,
   fetchFromGitHub,
-  cmake,
-  gtest,
-  msgpack,
-  libxml2,
-  python3,
-  python3Packages,
-  cudaPackages,
-  nlohmann_json,
-  triton-llvm,
-  lapack-reference,
-  ninja,
-  ncurses,
-  libffi,
-  zlib,
-  zstd,
-  xz,
-  pkg-config,
-  rocmPackages,
-  writeShellScriptBin,
-  buildTests ? false,
-  buildBenchmarks ? false,
-  buildSamples ? false,
-  gpuTargets ? [
-    # aotriton GPU support list:
-    # https://github.com/ROCm/aotriton/blob/main/v2python/gpu_targets.py
-    "gfx90a"
-    "gfx942"
-    "gfx1100"
-    "gfx1101"
-  ],
 }:
 
-stdenv.mkDerivation (
-  finalAttrs:
-  let
-    py = python3.withPackages (ps: [
-      ps.pyyaml
-      ps.distutils
-      ps.setuptools
-      ps.packaging
-      ps.numpy
-      ps.wheel
-      ps.filelock
-      ps.iniconfig
-      ps.pluggy
-      ps.pybind11
-    ]);
-    gpuTargets' = lib.concatStringsSep ";" gpuTargets;
-    compiler = "amdclang++";
-    cFlags = "-O3 -DNDEBUG";
-    triton-llvm' = triton-llvm;
-  in
-  {
-    pname = "aotriton";
-    version = "0.8.0b";
+let
+  generic = callPackage ./generic.nix { };
+in
+{
+  aotriton_0_8 = generic rec {
+    version = "0.8.2b";
 
     src = fetchFromGitHub {
       owner = "ROCm";
       repo = "aotriton";
-      rev = "0.8b";
-      hash = "sha256-C5Qr0EgV+pU6Hnmxqy76Nmryqr7qNkoE6iDcg9z35Hk=";
+      rev = "${version}";
+      hash = "sha256-gSzGYWfyUNLyzqpu3BM8rjFFL7cRVZ+w9L5pnh9QGz4=";
       fetchSubmodules = true;
     };
-    env.CXX = compiler;
-    env.ROCM_PATH = "${rocmPackages.clr}";
-    requiredSystemFeatures = [ "big-parallel" ];
 
-    outputs =
-      [
-        "out"
-      ]
-      ++ lib.optionals buildTests [
-        "test"
-      ]
-      ++ lib.optionals buildBenchmarks [
-        "benchmark"
-      ]
-      ++ lib.optionals buildSamples [
-        "sample"
-      ];
+    gpuTargets = [
+      # aotriton GPU support list:
+      # https://github.com/ROCm/aotriton/blob/main/v2python/gpu_targets.py
+      "gfx90a"
+      "gfx942"
+      "gfx1100"
+      "gfx1101"
+    ];
+  };
 
-    # ROCM doesn't include an empty cuda.h?
-    # It does in AMD's distribution
-    # :confused:
-    # https://github.com/pytorch/pytorch/issues/42430
-    postPatch = ''
-      echo "" > third_party/triton/third_party/nvidia/include/cuda.h
-    '';
+  aotriton_0_9 = generic rec {
+    version = "0.9.2b";
 
-    doCheck = false;
-    doInstallCheck = false;
+    src = fetchFromGitHub {
+      owner = "ROCm";
+      repo = "aotriton";
+      rev = version;
+      hash = "sha256-1Cf0olD3zRg9JESD6s/WaGifm3kfD12VUvjTZHpmGAE=";
+      fetchSubmodules = true;
+    };
 
-    nativeBuildInputs = [
-      cmake
-      rocmPackages.rocm-cmake
-      pkg-config
-      py
-      rocmPackages.clr
-      #git
-      #gfortran
-      ninja
-      (writeShellScriptBin "amdclang++" ''
-        exec ${rocmPackages.llvm.clang}/bin/clang++ "$@"
-      '')
+    patches = [
+      # This was not an issue in 0.8.0b, but appeared when updating to 0.9.xb:
+      #   error: non-constant-expression cannot be narrowed from type
+      #   'int32_t' (aka 'int') to 'uint32_t' (aka 'unsigned int') in
+      #   initializer list [-Wc++11-narrowing]
+      ./explicit-cast-for-narrowing.diff
+      # Fails with: ld.lld: error: unable to insert .comment after .comment
+      ./no-ld-script.diff
     ];
 
-    buildInputs =
-      [
-        libffi
-        ncurses
-        xz
-        nlohmann_json
-        #rocmlir
-        #cudaRtIncludes
-
-        # Tensile deps - not optional, building without tensile isn't actually supported
-        msgpack # FIXME: not included in cmake!
-        libxml2
-        python3Packages.msgpack
-        zlib
-        zstd
-        #python3Packages.joblib
-      ]
-      ++ (with rocmPackages; [
-        hipblas
-        openmp
-        rocblas
-        rocsolver
-      ])
-      ++ lib.optionals buildTests [
-        gtest
-      ]
-      ++ lib.optionals (buildTests || buildBenchmarks) [
-        lapack-reference
-      ];
-
-    env.TRITON_OFFLINE_BUILD = 1;
-    env.LLVM_SYSPATH = "${triton-llvm'}";
-    env.JSON_SYSPATH = nlohmann_json;
-    env.MLIR_DIR = "${triton-llvm'}/lib/cmake/mlir";
-    # build time dep for header only, only needs source.
-    # env.TRITON_CUDACRT_PATH = cudaRtIncludes;
-    # env.TRITON_CUDART_PATH = cudaRtIncludes;
-    env.CXXFLAGS = "-I/build/source/third_party/triton/third_party/nvidia/backend/include";
-    # env.NOIMAGE_MODE = 1;
-
-    # Fix up header issues in triton: https://github.com/triton-lang/triton/pull/3985/files
-    preConfigure = ''
-      mkdir third_party/triton/third_party/nvidia/backend/include/
-      touch third_party/triton/third_party/nvidia/backend/include/cuda.h
-      #cp ''${cudaRtIncludes}/include/*.h third_party/triton/third_party/nvidia/backend/include/
-      find third_party/triton -type f -exec sed -i 's|[<]cupti.h[>]|"cupti.h"|g' {} +
-      find third_party/triton -type f -exec sed -i 's|[<]cuda.h[>]|"cuda.h"|g' {} +
-      grep -ir cuda.h third_party/triton
-      find third_party/triton -name 'cuda.h'
-      # echo $TRITON_CUDACRT_PATH
-      # ls $TRITON_CUDACRT_PATH
-      # exit 1
-      sed -i '2s;^;set(CMAKE_SUPPRESS_DEVELOPER_WARNINGS ON CACHE BOOL "ON")\n;' CMakeLists.txt
-      sed -i '2s;^;set(CMAKE_VERBOSE_MAKEFILE ON CACHE BOOL "ON")\n;' CMakeLists.txt
-      sed -i '2s;^;set(CMAKE_SUPPRESS_DEVELOPER_WARNINGS ON CACHE BOOL "ON")\n;' third_party/triton/CMakeLists.txt
-      sed -i '2s;^;set(CMAKE_VERBOSE_MAKEFILE ON CACHE BOOL "ON")\n;' third_party/triton/CMakeLists.txt
-      substituteInPlace third_party/triton/python/setup.py \
-        --replace-fail "from distutils.command.clean import clean" "import setuptools;from distutils.command.clean import clean" \
-        --replace-fail 'system == "Linux"' 'False'
-      # sed -i 's|^download_and_copy|dict|g' third_party/triton/python/setup.py
-      cmakeFlagsArray+=(
-        '-DCMAKE_C_FLAGS_RELEASE=${cFlags}'
-        '-DCMAKE_CXX_FLAGS_RELEASE=${cFlags}'
-      )
-      prependToVar cmakeFlags "-GNinja"
-      mkdir -p /build/tmp-home
-      export HOME=/build/tmp-home
-    '';
-
-    # From README:
-    # Note: do not run ninja separately, due to the limit of the current build system,
-    # ninja install will run the whole build process unconditionally.
-    buildPhase = ''
-      echo "Skipping build phase due to aotriton bug"
-    '';
-
-    installPhase = ''
-      ninja -v install
-    '';
-
-    cmakeFlags =
-      [
-        #"--debug"
-        #"--trace"
-        "-Wno-dev"
-        "-DAOTRITON_NOIMAGE_MODE=ON" # FIXME: Should be able to build with object code but generate_shim is failing
-        "-DCMAKE_BUILD_TYPE=Release"
-        "-DCMAKE_VERBOSE_MAKEFILE=ON"
-        # "-DCMAKE_CXX_COMPILER=hipcc" # MUST be set because tensile uses this
-        # "-DCMAKE_C_COMPILER=${lib.getBin clr}/bin/hipcc"
-        "-DVIRTUALENV_PYTHON_EXENAME=${lib.getExe py}"
-        "-DCMAKE_CXX_COMPILER=${compiler}"
-        # Manually define CMAKE_INSTALL_<DIR>
-        # See: https://github.com/NixOS/nixpkgs/pull/197838
-        "-DCMAKE_INSTALL_BINDIR=bin"
-        "-DCMAKE_INSTALL_LIBDIR=lib"
-        "-DCMAKE_INSTALL_INCLUDEDIR=include"
-        "-DAMDGPU_TARGETS=${gpuTargets'}"
-        "-DGPU_TARGETS=${gpuTargets'}"
-      ]
-      ++ lib.optionals buildTests [
-        "-DBUILD_CLIENTS_TESTS=ON"
-      ]
-      ++ lib.optionals buildBenchmarks [
-        "-DBUILD_CLIENTS_BENCHMARKS=ON"
-      ]
-      ++ lib.optionals buildSamples [
-        "-DBUILD_CLIENTS_SAMPLES=ON"
-      ];
-
-    postInstall =
-      lib.optionalString buildTests ''
-        mkdir -p $test/bin
-        mv $out/bin/hipblas-test $test/bin
-      ''
-      + lib.optionalString buildBenchmarks ''
-        mkdir -p $benchmark/bin
-        mv $out/bin/hipblas-bench $benchmark/bin
-      ''
-      + lib.optionalString buildSamples ''
-        mkdir -p $sample/bin
-        mv $out/bin/example-* $sample/bin
-      ''
-      + lib.optionalString (buildTests || buildBenchmarks || buildSamples) ''
-        rmdir $out/bin
-      '';
-    meta = with lib; {
-      description = "Ahead of Time (AOT) Triton Math Library";
-      homepage = "https://github.com/ROCm/aotriton";
-      license = with licenses; [ mit ];
-      platforms = platforms.linux;
-    };
-  }
-)
+    gpuTargets = [
+      "gfx90a"
+      "gfx942"
+      "gfx950"
+      "gfx1100"
+      "gfx1101"
+      "gfx1201"
+    ];
+  };
+}
