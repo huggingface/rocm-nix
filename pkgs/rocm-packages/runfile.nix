75a7b1ffb52ce3aa08e863a1db58721acd7afb90
Daniël de Kok <me@danieldk.eu>
2025-03-17 16:25:34 +0000

    rocmPackages: 6.3.2 -> 6.3.3

--- pkgs/rocm-packages/runfile.nix
+++ pkgs/rocm-packages/runfile.nix
@@ -3,17 +3,17 @@
 }:
 
 let
-  version = "6.3.2";
+  version = "6.3.3";
 in
 fetchurl {
   name = "rocm-bundle";
-  url = "https://repo.radeon.com/rocm/installer/rocm-runfile-installer/rocm-rel-${version}/ubuntu/22.04/rocm-installer_1.0.0.60302-7~22.04.run";
+  url = "https://repo.radeon.com/rocm/installer/rocm-runfile-installer/rocm-rel-${version}/ubuntu/22.04/rocm-installer_1.0.1.60303-9~22.04.run";
 
   # Make the extracted runfile a fixed-output derivation. This avoids
   # unpacking the runfile during every build.
   recursiveHash = true;
   downloadToTemp = true;
-  hash = "sha256-cB4v8Jfwnuhms5OPp26yg5tF+MY/vrvVK4IFSmcyjkg=";
+  hash = "sha256-74Ms/3cfwHGD4mM4XEG+FCHqlKI0qbd73JKbLD7MqkY=";
 
   postFetch = ''
     SETUP_NOCHECK=1 sh "$downloadedFile" --noexec --target $out
